537a2287cba2cfaa985dce81ce86a04452a946a5
Jehoi-ga-ada <sayajehoiada@gmail.com>
2026-02-28 19:22:39 +0700

    feat(react): function calling reasoning

---
+++ react.py
@@ -0,0 +1,25 @@
+from dotenv import load_dotenv
+from langchain_core.tools import tool
+from langchain_ollama import ChatOllama
+from langchain_tavily import TavilySearch
+
+load_dotenv()
+
+@tool
+def triple(num: float) -> float:
+    """
+    Triples the input number
+    
+    Args:
+        num (float): a number to triple
+
+    Returns:
+        float: the triple of the input number
+    """
+    
+    return float(num) * 3
+
+tools = [TavilySearch(max_results=1), triple]
+
+llm = ChatOllama(model="qwen3:1.7b", temperature=0).bind_tools(tools=tools)
+
