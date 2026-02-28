e8f69ad5d4b66cec7a732642e901b351ade209f0
Jehoi-ga-ada <sayajehoiada@gmail.com>
2026-02-28 19:29:47 +0700

    style(react): formatting

--- react.py
+++ react.py
@@ -5,21 +5,22 @@ from langchain_tavily import TavilySearch
 
 load_dotenv()
 
+
 @tool
 def triple(num: float) -> float:
     """
     Triples the input number
-    
+
     Args:
         num (float): a number to triple
 
     Returns:
         float: the triple of the input number
     """
-    
+
     return float(num) * 3
+
 
 tools = [TavilySearch(max_results=1), triple]
 
 llm = ChatOllama(model="qwen3:1.7b", temperature=0).bind_tools(tools=tools)
-
